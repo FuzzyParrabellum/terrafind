bf5082061229e2e9bfdfd7936f6d574826e59a12
Rodriguez antoine <rdzantoine.pro@.com>
2026-04-22 11:29:01 +0200

    feat: add new filter to VenteParcelle Django admin view list

--- backend/appsfolio/transaction/admin.py
+++ backend/appsfolio/transaction/admin.py
@@ -3,6 +3,32 @@ from django.contrib import admin
 from .models import VenteParcelle
 
 
+class TypeLocalFilter(admin.SimpleListFilter):
+    """
+    Filtre admin pour types_locaux (ArrayField PostgreSQL).
+
+    list_filter standard génère WHERE types_locaux = 'Maison', ce qui ne
+    fonctionne pas sur un tableau. On surcharge queryset() pour utiliser
+    __contains, qui génère WHERE types_locaux @> ARRAY['Maison'].
+    """
+    title = "type de bien"       # libellé affiché dans le panneau de droite
+    parameter_name = "type_local"  # nom du paramètre dans l'URL de l'admin
+
+    def lookups(self, _request, _model_admin):
+        # Chaque tuple : (valeur utilisée dans l'URL, libellé affiché).
+        # On utilise les choix définis sur le modèle pour rester cohérent.
+        return [
+            (choice.value, choice.label)
+            for choice in VenteParcelle.TypeLocal
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            # __contains sur ArrayField vérifie que le tableau inclut cette valeur.
+            return queryset.filter(types_locaux__contains=[self.value()])
+        return queryset
+
+
 @admin.register(VenteParcelle)
 class VenteParcelleAdmin(admin.ModelAdmin):
     list_display = (
@@ -16,7 +42,7 @@ class VenteParcelleAdmin(admin.ModelAdmin):
         "surface_totale",
         "nombre_pieces_principales",
     )
-    list_filter = ("nature_mutation", "nature_culture", "commune")
+    list_filter = ("nature_mutation", "nature_culture", TypeLocalFilter, "commune")
     search_fields = ("commune__nom", "commune__code_insee", "code_postal")
     ordering = ("-date_mutation",)
     readonly_fields = ("public_id",)
